6502204a05c61a56c40c9e6513d7ad97d4366028
DioPires <diogo.m.pires@tecnico.ulisboa.pt>
2018-03-14 12:44:35 +0000

    Added event Buy to AppCoinsIAB

--- deploy/contracts/AppCoinsIAB.sol
+++ deploy/contracts/AppCoinsIAB.sol
@@ -17,29 +17,31 @@ contract AppCoinsIAB is AppCoinsIABInterface {
     uint public appstore_share = 10;
     uint public oem_share = 5;
 
+    event Buy(uint _amount, string _sku, address _from, address _dev, address _appstore, address _oem);
+
     function division(uint numerator, uint denominator) public constant returns (uint) {
-	uint _quotient = numerator / denominator;
-	return _quotient;
+        uint _quotient = numerator / denominator;
+        return _quotient;
     }
 
     function buy(uint256 _amount, string _sku, address _addr_appc, address _dev, address _appstore, address _oem) public constant returns (bool) {
         require(_addr_appc != 0x0);
-	require(_dev != 0x0);
-	require(_appstore != 0x0);
-	require(_oem != 0x0);
+        require(_dev != 0x0);
+        require(_appstore != 0x0);
+        require(_oem != 0x0);
 
         AppCoins2 appc2 = AppCoins2(_addr_appc);
-	uint256 aux = appc2.allowance(msg.sender, address(this));
-	require(aux >= _amount);
+        uint256 aux = appc2.allowance(msg.sender, address(this));
+        require(aux >= _amount);
 
         uint[] memory amounts = new uint[](3);
-	amounts[0] = division(_amount * dev_share, 100);
-	amounts[1] = division(_amount * appstore_share, 100);
-	amounts[2] = division(_amount * oem_share, 100);
+        amounts[0] = division(_amount * dev_share, 100);
+        amounts[1] = division(_amount * appstore_share, 100);
+        amounts[2] = division(_amount * oem_share, 100);
 
         appc2.transferFrom(msg.sender, _dev, amounts[0]);
-	appc2.transferFrom(msg.sender, _appstore, amounts[1]);
-	appc2.transferFrom(msg.sender, _oem, amounts[2]);
+        appc2.transferFrom(msg.sender, _appstore, amounts[1]);
+        appc2.transferFrom(msg.sender, _oem, amounts[2]);
 
         Buy(_amount, _sku, msg.sender, _dev, _appstore, _oem);
 
