c8393cc726e151e06f514c0cc2adb02f41cac6b6
Tiago Rosado <tiago.rosado@aptoide.com>
2018-03-26 13:34:20 +0100

    Created Advertisemnte contract, and createCampaign function

---
+++ deploy/contracts/Advertisement.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.8;
+/**
+ * The AdCampaign contract does this and that...
+ */
+contract Advertisement {
+
+	struct Filters {
+		string countries; 
+		string packageName;
+		uint[] vercodes;
+	}
+
+	struct ValidationRules {
+		bool vercode;
+		bool ipValidation;
+		bool country;
+		uint constipDailyConversions;
+		uint walletDailyConversions;
+	}
+
+	struct Campaign {	
+		bytes8 bid_id;
+		uint price;
+		uint budget;
+		uint startDate;
+		uint endDate;
+		string ipValidator;
+		address  owner;
+		Filters filters;
+	}
+
+	ValidationRules public rules;
+	mapping (bytes => Campaign[]) campaigns;
+
+	event CampaignCreated(string packageName, string countries, 
+							uint[] vercodes, uint price, uint budget,
+							uint startDate, uint endDate);
+
+	function Advertisement () public {
+		rules = ValidationRules(false,true,true,2,1);
+	}
+
+
+	function createCampaign (string packageName, string countries, 
+							uint[] vercodes, uint price, 
+							uint startDate, uint endDate) public payable {
+		Campaign memory newCampaign;
+		newCampaign.filters.packageName = packageName;
+		newCampaign.filters.countries = countries;
+		newCampaign.filters.vercodes = vercodes;
+		newCampaign.price = price;
+		newCampaign.startDate = startDate;
+		newCampaign.endDate = endDate;
+		
+		//Transfers the budget to contract address
+		newCampaign.budget = msg.value;
+		newCampaign.owner = msg.sender;
+
+		bytes memory country;
+		bytes memory countriesInBytes = bytes(countries);
+		uint countryLength = 0;
+		for (uint i=0; i<countriesInBytes.length; i++){
+
+			if(countriesInBytes[i]==','){
+				//if ',' is found, new String ahead
+				campaigns[country].push(newCampaign);
+				delete country;
+			} else {
+				country[countryLength]=countriesInBytes[i];
+				countryLength++;
+			}
+
+		}
+		
+		CampaignCreated(
+			packageName,
+			countries,
+			vercodes,
+			price,
+			newCampaign.budget,
+			startDate,
+			endDate);
+	}
+	
+}
--- deploy/migrations/2_deploy_contracts.js
+++ deploy/migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var AppCoin = artifacts.require("./AppCoin.sol");
 var AppCoin2 = artifacts.require("./AppCoin2.sol");
 var AppCoinsIAB = artifacts.require("./AppCoinsIAB.sol");
+var Advertisement = artifacts.require("./Advertisement.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(AppCoin);
     deployer.deploy(AppCoin2);
     deployer.deploy(AppCoinsIAB);
+    deployer.deploy(Advertisement);
 };
