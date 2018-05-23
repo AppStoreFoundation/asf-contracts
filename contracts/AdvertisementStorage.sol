50421f28d3b0b656bd99a3ed40ee27a44761d8c0
Gustavo Silva <gustavo.silva@aptoide.com>
2018-05-23 13:02:23 +0100

    [new] merge with dev

---
+++ contracts/AdvertisementStorage.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.19;
+
+import  { CampaignLibrary } from "./lib/CampaignLibrary";
+
+/**
+ * The Advertisement contract collects campaigns registered by developers
+ * and executes payments to users using campaign registered applications
+ * after proof of Attention.
+ */
+contract AdvertisementStorage {
+
+    mapping (bytes32 => CampaignLibrary.Campaign) campaigns;
+
+    function getCampaign (byte32 campaignId) public view returns (CampaignLibrary.Campaign) {
+        return campaigns[campaignId];
+    }
+
+
+    function setCampaign (byte32 campaignId, CampaignLibrary.Campaign campaign) public view returns (CampaignLibrary.Campaign) {
+        return campaigns[campaignId] = campaign;
+    }
+}
