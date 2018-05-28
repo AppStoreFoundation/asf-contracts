ee828b543f1f73041aea2925717854af6fec2004
Gustavo Silva <gustavo.silva@aptoide.com>
2018-05-28 16:14:41 +0100

    [new] decoupled the migration to a single file for cotnract

---
+++ migrations/3_deploy_app_coins_iab.js
@@ -0,0 +1,42 @@
+var AppCoinsIAB = artifacts.require("./AppCoinsIAB.sol");
+
+require('dotenv').config();
+
+module.exports = function(deployer, network) {
+    switch (network) {
+        case 'development':
+            deployer.deploy(AppCoinsIAB);
+            break;
+
+        case 'ropsten':
+            var AppCoinsIABAddress = process.env.IAB_ROPSTEN_ADDRESS;
+
+            if (!AppCoinsIABAddress) {
+                deployer.deploy(AppCoinsIAB);
+            }
+
+            break;
+
+        case 'kovan':
+            var AppCoinsIABAddress = process.env.IAB_KOVAN_ADDRESS;
+
+            if (!AppCoinsAddress) {
+                deployer.deploy(AppCoinsIAB);
+            }
+
+            break;
+
+        case 'main':
+            var AppCoinsIABAddress = process.env.IAB_MAINNET_ADDRESS;
+
+            if (!AppCoinsAddress) {
+                deployer.deploy(AppCoinsIAB);
+            }
+
+            break;
+
+        default:
+            throw `Unknown network "${network}". See your Truffle configuration file for available networks.` ;
+
+    }
+};
