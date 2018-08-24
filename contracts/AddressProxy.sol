959981de891ca1eaf5bde282c53ed2b1785d0724
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-08-24 11:01:13 +0100

    docs(AddressProxy): Add documentation comments to smart contract's code

--- contracts/AddressProxy.sol
+++ contracts/AddressProxy.sol
@@ -1,5 +1,11 @@
 pragma solidity ^0.4.19;
 
+/**
+@title AddressProxy contract
+@author App Store Foundation
+@dev This contract works as part of a set of mechanisms in order to maintain tracking of the latest
+version's contracts deployed to the network.
+ */
 
 contract AddressProxy {
 
@@ -27,13 +33,22 @@ contract AddressProxy {
         owner = msg.sender;
     }
 
+
+    /**
+    @notice Get all avaliable ids registered on the contract
+    @dev Just shows the list of ids registerd on the contract
+    @return { "Id List" : "List of registered ids" }
+     */
     function getAvailableIds() public view returns (bytes32[]) {
         return availableIds;
     }
 
-    //  Adds or updates an address
-    //  @params {string} name - the name of the contract Address
-    //  @params {address} newAddress
+    /** 
+    @notice  Adds or updates an address
+    @dev Used when a new address needs to be updated to a currently registered id or to a new id.
+    @param name Name of the contract
+    @param newAddress Address of the contract
+    */
     function addAddress(string name, address newAddress) public onlyOwner {
         bytes32 contAddId = stringToBytes32(name);
 
@@ -59,24 +74,50 @@ contract AddressProxy {
         }
     }
 
+    /**
+    @notice Get the contract name associated to a certain id
+    @param id Id of the registry
+    @return { 'name' : 'Name of the contract associated to the given id' }
+     */
     function getContractNameById(bytes32 id) public view returns(string) {
         return contractsAddress[id].name;
     }
 
+
+    /**
+    @notice Get the contract address associated to a certain id
+    @param id Id of the registry
+    @return { 'address' : 'Address of the contract associated to the given id' }
+     */
     function getContractAddressById(bytes32 id) public view returns(address) {
         return contractsAddress[id].at;
     }
 
+    /**
+    @notice Get the specific date on which the contract address was firstly registered 
+    to a certain id
+    @param id Id of the registry
+    @return { 'time' : 'Time in miliseconds of the first time the given id was registered' }
+     */
     function getContractCreatedTimeById(bytes32 id) public view returns(uint) {
         return contractsAddress[id].createdTime;
     }
 
+    /**
+    @notice Get the specific date on which the contract address was lastly updated to a certain id
+    @param id Id of the registry
+    @return { 'time' : 'Time in miliseconds of the last time the given id was updated' }
+     */
     function getContractUpdatedTimeById(bytes32 id) public view returns(uint) {
         return contractsAddress[id].updatedTime;
     }
 
-    //  @params {string} source
-    //  @return {bytes32}
+    /**
+    @notice Converts a string type variable into a byte32 type variable
+    @dev This function is internal and uses inline assembly instructions.
+    @param source string to be converted to a byte32 type
+    @return { 'result' : 'Initial string content converted to a byte32 type' }
+     */
     function stringToBytes32(string source) internal pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
