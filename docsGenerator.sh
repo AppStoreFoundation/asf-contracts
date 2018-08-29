52cffd48ab047c3c395e1f365d4e0697e1bf8d38
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-08-29 13:26:52 +0100

    docs(script): Add final script for doc generation

--- docsGenerator.sh
+++ docsGenerator.sh
@@ -2,11 +2,27 @@
 cont="/contracts"
 path="$( $(dirname $0) ; pwd -P)"
 echo $path
+cd $path
+
+homepage=$(grep -o "\"homepage.*" package.json)
+url=$(grep -o "\"url.*" package.json )
+author=$(grep -o "\"author.*" package.json)
+name=$(grep -o "\"name.*" package.json)
+desc=$(grep -o "\"desc.*" package.json)
+version=$(grep -o "\"version.*" package.json)
+
 cd /tmp
 git clone git@github.com:RyanHendricks/doxity-simpleton.git
 cd doxity-simpleton
 
-echo $(pwd)
+awk -v na="$version" '{gsub(/\"version\":.*?/,na);}1' package.json > package.tmp && mv package.tmp package.json
+awk -v na="$homepage" '{gsub(/\"homepage\":.*?/,na);}1' package.json > package.tmp && mv package.tmp package.json
+awk -v na="$url" '{gsub(/\"url\":.*?/,na);}1' package.json > package.tmp && mv package.tmp package.json
+awk -v na="$author" '{gsub(/\"author\":.*?/,na);}1' package.json > package.tmp && mv package.tmp package.json
+awk -v na="$name" '{gsub(/\"name\":.*?/,na);}1' package.json > package.tmp && mv package.tmp package.json
+awk -v na="$desc" '{gsub(/\"description\":.*?/,na);}1' package.json > package.tmp && mv package.tmp package.json
+
+cp $path/README.md .
 rm -rf ./contracts/*
 cp -r $path$cont/* ./contracts
 mv ./contracts/lib/* ./contracts/.
@@ -27,15 +43,3 @@ cp -rf doxity $path
 
 cd /tmp
 rm -rf /tmp/doxity-simpleton
-# set repo folder
-#sed  -i 's/^linkPrefix .*$/linkPrefix = "/asf-contracts"/g' ./doxity/config.toml
-# set name
-#sed  -i 's/^name .*$/name = "asf-contracts"/g' ./doxity/config.toml
-# set author
-#sed  -i 's/^author .*$/description = "App Store Foundation"/g' ./doxity/config.toml
-# set description
-#sed  -i 's/^homepage .*$/homepage = "https://github.com/AppStoreFoundation/asf-contracts.git"/g' ./doxity/config.toml
-
-
-
-#npm run-script develop -y
