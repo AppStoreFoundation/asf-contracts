b2658f5fc3b74e92d8cc304a1571094a104552ca
Gustavo Silva <gustavo.silva@aptoide.com>
2018-09-25 17:01:37 +0100

    [new] Timelock

--- contracts/AppCoinsTimelock.sol
+++ contracts/AppCoinsTimelock.sol
@@ -10,70 +10,70 @@ import "./AppCoins.sol";
  */
 contract AppCoinsTimelock {
 
-  // AppCoins token
-  AppCoins private appc;
+    // AppCoins token
+    AppCoins private appc;
 
-  // beneficiary of tokens before they are released
-  mapping (address => uint) balances;
+    // beneficiary of tokens before they are released
+    mapping (address => uint) balances;
 
-  // timestamp when token release is enabled
-  uint private releaseTime;
+    // timestamp when token release is enabled
+    uint private releaseTime;
 
-  event NewFundsAllocated(address _address,  uint _amount);
+    event NewFundsAllocated(address _address,  uint _amount);
 
-  constructor(
-    address _addrAppc,
-    uint256 _releaseTime
-  )
+    constructor(
+        address _addrAppc,
+        uint256 _releaseTime
+    )
     public
-  {
-    appc = AppCoins(_addrAppc);
-    releaseTime = _releaseTime;
-  }
-
-  /**
-   * @return the time when the tokens are released.
-   */
-  function getReleaseTime() public view returns(uint256) {
-    return releaseTime;
-  }
-
-  /**
-   * @dev get the address' balances
-   */
-  function getBalanceOf(address _address) public view returns(uint256){
-      return balances[_address];
-  }
-
-  /**
-   * @notice Transfers tokens held by timelock to beneficiary.
-   */
-  function allocateFunds(address _address, uint256 amount) public {
-    require(appc.allowance(msg.sender, address(this)) >= amount);
-    appc.transferFrom(msg.sender, address(this), amount);
-    balances[_address] = balances[_address] + amount;
-    emit NewFundsAllocated(_address, balances[_address]);
-  }
-
-  /**
-   * @notice Transfers tokens held by timelock to beneficiary.
-   */
-  function allocateFundsBulk(address[] _addresses, uint256[] amounts) public {
-    require(_addresses.length == amounts.length);
-    for(uint i = 0; i < _addresses.length; i++){
-        allocateFunds(_addresses[i], amounts[i]);
+    {
+        appc = AppCoins(_addrAppc);
+        releaseTime = _releaseTime;
     }
-  }
 
-  /**
-   * @notice Transfers tokens held by timelock to beneficiary.
-   */
-  function release(address _address) public {
-    uint nowInMilliseconds = block.timestamp * 1000;
+    /**
+    * @return the time when the tokens are released.
+    */
+    function getReleaseTime() public view returns(uint256) {
+        return releaseTime;
+    }
 
-    require(nowInMilliseconds >= releaseTime);
+    /**
+    * @dev get the address' balances
+    */
+    function getBalanceOf(address _address) public view returns(uint256){
+        return balances[_address];
+    }
 
-    appc.transfer(_address, balances[_address]);
-    balances[_address] = 0;
-  }
+    /**
+    * @notice Transfers tokens held by timelock to beneficiary.
+    */
+    function allocateFunds(address _address, uint256 amount) public {
+        require(appc.allowance(msg.sender, address(this)) >= amount);
+        appc.transferFrom(msg.sender, address(this), amount);
+        balances[_address] = balances[_address] + amount;
+        emit NewFundsAllocated(_address, balances[_address]);
+    }
+
+    /**
+    * @notice Transfers tokens held by timelock to beneficiary.
+    */
+    function allocateFundsBulk(address[] _addresses, uint256[] amounts) public {
+        require(_addresses.length == amounts.length);
+        for(uint i = 0; i < _addresses.length; i++){
+            allocateFunds(_addresses[i], amounts[i]);
+        }
+    }
+
+    /**
+    * @notice Transfers tokens held by timelock to beneficiary.
+    */
+    function release(address _address) public {
+        uint nowInMilliseconds = block.timestamp * 1000;
+
+        require(nowInMilliseconds >= releaseTime);
+
+        appc.transfer(_address, balances[_address]);
+        balances[_address] = 0;
+    }
 }
