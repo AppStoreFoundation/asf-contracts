c570525658be7434619b741279264a02152ad454
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2018-10-18 10:57:30 +0100

    docs(Extended Finance): Add documentation to new functions

--- contracts/ExtendedFinance.sol
+++ contracts/ExtendedFinance.sol
@@ -37,14 +37,27 @@ contract ExtendedFinance is BaseFinance {
 
     }
 
+    /**
+    @notice Withdraws user's rewards
+    @dev
+        Function to transfer a certain user's rewards to his address 
+    @param _user Address who's rewards will be withdrawn
+    @param _value Value of the withdraws which will be transfered to the user 
+    */
     function withdrawRewards(address _user, uint256 _value) public onlyOwnerOrAllowed {
         require(rewardedBalance[_user] >= _value);
 
         appc.transfer(_user, _value);
         rewardedBalance[_user] -= _value;
     }
-
-    function getRewardsBalance(address _user) public onlyOwnerOrAllowed returns (uint256) {
+    /**
+    @notice Get user's rewards balance
+    @dev
+        Function returning a user's rewards balance not yet withdrawn
+    @param _user Address of the user
+    @return { "_balance" : "Rewards balance of the user" }
+    */
+    function getRewardsBalance(address _user) public onlyOwnerOrAllowed returns (uint256 _balance) {
         return rewardedBalance[_user];
     }
 
